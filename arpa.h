9041329698e626803bb4ae18c4f8c572d3957f98
Aswini S <ashumeow@live.com>
2014-01-28 13:09:11 +0530

    Update arpa.h

--- arpa.h
+++ arpa.h
@@ -6,3 +6,9 @@ returns 0 if error */
 #define in_addr_t inet_addr(const char *strptr)
 /*returns pointer to dotted decimal string */
 char *inet_ntoa struct in_addr inaddr
+
+#define inet_pton()
+	/* returns 1 if OK, 0 if input not a valid presentation format, -1 if error */
+inet_pton(int family, const char *strptr, void *addrptr)
+/*returns pointer to result if OK and if error, then returns NULL */
+const char *inet_ntop(int family, const void *addrptr, char *strptr, size_t len)
