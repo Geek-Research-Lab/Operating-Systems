e046990accac83df8627fcc2b7bbf9a3e1cf16a1
Aswini S <ashumeow@live.com>
2014-02-20 14:01:02 +0530

    Create multithreading_proj.h

---
+++ multithreading_proj.h
@@ -0,0 +1,7 @@
+#ifndef MULTITHREADING_PROJ_H
+#define MULTITHREADING_PROJ_H
+#include<Rpc.h>
+#define SHELLPROG((unsigned long)(0X20000001))
+#define SHELLVER ((unsigned long)(1))
+#define EXECSHELL ((unsigned long)(1))
+#endif
